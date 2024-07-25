c6cb34faa1c121e0c7a4b432197e527a97900708
admin <admin@ghurcka.local>
2024-07-25 15:07:10 -0400

    day 17 finish

--- day-017/quiz-game-start/main.py
+++ day-017/quiz-game-start/main.py
@@ -1,8 +1,15 @@
 from data import question_data
 from question_model import Question
+from quiz_brain import QuizBrain
 
 question_bank = []
 for _ in question_data:
     question_bank.append(Question(_["text"],_["answer"]))
 
-print(question_bank[0].text)
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("You complete quiz.")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
